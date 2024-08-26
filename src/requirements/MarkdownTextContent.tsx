de52a533eb344130bcf86557038b7531173d15ab
holenet <holenet6147@naver.com>
2024-08-26 15:05:39 +0900

    fix: open markdown link on new tab

--- src/requirements/MarkdownTextContent.tsx
+++ src/requirements/MarkdownTextContent.tsx
@@ -2,6 +2,11 @@ import { useEffect, useRef } from "preact/hooks";
 import { RequirementMessage } from "../model";
 import { marked } from "marked";
 
+const renderer = new marked.Renderer();
+renderer.link = function ({ href, title, text }) {
+  return '<a target="_blank" href="' + href + '" title="' + (title ?? text) + '">' + text + "</a>";
+};
+
 type Props = {
   message: RequirementMessage;
 };
@@ -13,6 +18,7 @@ export function MarkdownTextContent({ message }: Props) {
       message.text + (message.requirement.hint ? ` (힌트: "${message.requirement.hint}")` : ""),
       {
         async: false,
+        renderer,
       }
     );
   }, [message.text, message.requirement.hint]);
